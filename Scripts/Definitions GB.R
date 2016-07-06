29baf86c614dc64f975c498101da20c1f217d5b6
mgorgol <Maciej.Gorgol@server1.lim.com.hk>
2016-07-06 19:29:57 +0800

    Dictionary Currently new english columns for raw data

---
+++ Scripts/Definitions GB.R
@@ -0,0 +1,12 @@
+###################################################################################################
+#  DEFINITIONS
+###################################################################################################
+
+train.col.names = c("Week", "Sales_depot", "Sales_channel", "Route", "Client_ID", 
+  "Product_ID", "Sales_units", "Sales_pesos", 
+  "NW_Returns_units", "NW_Returns_pesos",
+  "Adjusted_Demand")
+
+client.col.names     = c("Client_ID", "Client_name")
+product.col.names    = c("Product_ID", "Product_name")
+town.state.col.names = c("Sales_depot", "Town", "State")
